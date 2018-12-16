eab42b60a13a6173cbcb5fb08ae40b5701a4fddf
XZ-Dev <xiangzhedev@gmail.com>
2018-12-16 21:43:07 +0800

    fix bugs

--- wechat/chat/message.py
+++ wechat/chat/message.py
@@ -1,6 +1,8 @@
 from ..data.get import get_api
 
-__all__ = ['Message', ]
+__all__ = [
+    'Message',
+]
 
 
 class Message():
@@ -13,12 +15,16 @@ class Message():
     def send_message(self, str_message):
         if not self.__send_message_api:
             if self.__chat_object_type == 'friend':
-                self.__send_message_api = '/openqq/send_friend_message'
+                self.__send_message_api = '/openwx/send_friend_message'
             elif self.__chat_object_type == 'group':
-                self.__send_message_api = '/openqq/send_group_message'
+                self.__send_message_api = '/openwx/send_group_message'
         return self.__send_message(str_message)
 
-    def __send_message(self, str_message, is_https=0, host='127.0.0.1', port='5000'):
+    def __send_message(self,
+                       str_message,
+                       is_https=0,
+                       host='127.0.0.1',
+                       port='3000'):
         api = self.__send_message_api
         chat_object_id = self.__chat_object_id
         payload = {'id': chat_object_id, 'content': str_message}
@@ -28,5 +34,5 @@ class Message():
                 return True
             else:
                 return False
-        except:
+        except Exception:
             return None
--- wechat/data/__init__.py
+++ wechat/data/__init__.py
@@ -1,7 +1,7 @@
 import sys
-from . import data, data_handle
+from . import data_handle
 
-__all__ = ['data', 'data_handle', ]
+__all__ = ['data_handle', ]
 
 if '__main__' in sys.modules:
     sys.modules['__mp_main__'] = sys.modules['__main__']
